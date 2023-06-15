3d18130d6c7dd5e941c9dbaa4d3376607eba3cfd
lordyhas <dev.haspro@gmail.com>
2023-06-15 10:41:02 +0200

    version 0.1.6+5beta | doctor screen <- fluent ui

--- lib/src/add_doctor/doctor_data_tab.dart
+++ lib/src/add_doctor/doctor_data_tab.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/material.dart' show DataTableSource, DataRow, DataCell, DataColumn, PaginatedDataTable;
 import 'package:qmt_manager/src/add_doctor/add_doctor_page.dart';
 import 'package:qmt_manager/logic/data.csv.dart';
 
@@ -304,16 +305,16 @@ class DoctorDataSource extends DataTableSource {
   }
 }
 
-class DataTableDemo extends StatefulWidget {
-  const DataTableDemo({Key? key}) : super(key: key);
+class DoctorDataTableScreen extends StatefulWidget {
+  const DoctorDataTableScreen({Key? key}) : super(key: key);
 
   static const String routeName = '/data-table';
 
   @override
-  State<DataTableDemo> createState() => _DataTableDemoState();
+  State<DoctorDataTableScreen> createState() => _DoctorDataTableScreenState();
 }
 
-class _DataTableDemoState extends State<DataTableDemo> {
+class _DoctorDataTableScreenState extends State<DoctorDataTableScreen> {
   int _rowsPerPage = PaginatedDataTable.defaultRowsPerPage;
   int? _sortColumnIndex;
   bool _sortAscending = true;
@@ -330,110 +331,104 @@ class _DataTableDemoState extends State<DataTableDemo> {
 
   @override
   Widget build(BuildContext context) {
-    final ScrollController scrollController =
-        ScrollController(debugLabel: 'scrollDoctors');
-    return Scaffold(
-      backgroundColor: Colors.grey.shade900,
-      appBar: AppBar(
+    final ScrollController scrollController = ScrollController(
+      debugLabel: 'scrollDoctors',
+    );
+    return ScaffoldPage(
+      /*header: PageHeader(
         leading:  Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Image.asset("assets/icon_app_red.png", height: 32 ,)
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Image.asset("assets/icon_app_red.png", height: 32 ,)
         ), /*IconButton(
           icon: const Icon(Icons.close),
           onPressed: Go.of(context).pop,
         ),*/
         title: const Text('Doctor data tables'),
-      ),
-      body: Scrollbar(
-        controller: scrollController,
-        child: ListView(
-          controller: scrollController,
-          padding: const EdgeInsets.all(20.0),
-          children: <Widget>[
+      ),*/
 
-            PaginatedDataTable(
-              //
-              header: const Text('Doctors'),
-              actions: [
-                IconButton(onPressed: () {}, icon: const Icon(Icons.delete)),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.wifi_protected_setup,),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.add),
-                  onPressed: () {
-                    Go.of(context).to(
-                      routeName: AddDoctorPage.routeName,
-                    );
-                  }
-                ),
-              ],
-              rowsPerPage: _rowsPerPage,
-              onRowsPerPageChanged: (int? value) {
-                setState(() {
-                  _rowsPerPage = value!;
-                });
-              },
-              sortColumnIndex: _sortColumnIndex,
-              sortAscending: _sortAscending,
-              onSelectAll: _doctorsDataSource.selectAll,
-              availableRowsPerPage: const <int>[10,20,40,60],
-
-              columns: <DataColumn>[
-                DataColumn(
-                  label: const Text('Nom Complet'),
-                  onSort: (int columnIndex, bool ascending) => _sort<String>(
-                      (Doctor d) => d.name, columnIndex, ascending),
-                ),
-                DataColumn(
-                  label: const Text('Sex'),
-                  //numeric: true,
-                  onSort: (int columnIndex, bool ascending) => _sort<String>(
-                      (Doctor d) => d.sex, columnIndex, ascending),
-                ),
-                DataColumn(
-                  label: const Text('Speciality'),
-                  onSort: (int columnIndex, bool ascending) => _sort<String>(
-                      (Doctor d) => d.speciality, columnIndex, ascending),
-                ),
-                DataColumn(
-                  label: const Text('Hospital'),
-                  //tooltip: 'Each hospital.',
-                  onSort: (int columnIndex, bool ascending) => _sort<String>(
-                      (Doctor d) => d.hospital, columnIndex, ascending),
-                ),
-                DataColumn(
-                  label: const Text('Location'),
-                  tooltip: 'Hospital location.',
-                  onSort: (int columnIndex, bool ascending) => _sort<String>(
-                      (Doctor d) => d.location, columnIndex, ascending),
-                ),
-                DataColumn(
-                  label: const Text('Phone'),
-                  onSort: (int columnIndex, bool ascending) => _sort<String>(
-                      (Doctor d) => d.phoneNumbers.first,
-                      columnIndex,
-                      ascending),
-                ),
-                DataColumn(
-                  label: const Text('Email'),
-                  //tooltip: '',
-                  onSort: (int columnIndex, bool ascending) => _sort<String>(
-                      (Doctor d) => d.emails.first, columnIndex, ascending),
-                ),
-                DataColumn(
-                  label: const Text('Doctor'),
-                  numeric: true,
-                  onSort: (int columnIndex, bool ascending) => _sort<String>(
-                      (Doctor d) => d.isDoctor.toString(),
-                      columnIndex,
-                      ascending),
-                ),
-              ],
-              source: _doctorsDataSource,
+      content: SingleChildScrollView(
+        //controller: scrollController,
+        padding: const EdgeInsets.all(20.0),
+        child:  PaginatedDataTable(
+          header: const Text('Doctors'),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(FluentIcons.delete)),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(FluentIcons.o_d_link,),
+            ),
+            IconButton(
+                icon: const Icon(FluentIcons.add),
+                onPressed: () {
+                  Go.of(context).to(
+                    routeName: AddDoctorPage.routeName,
+                  );
+                }
             ),
           ],
+          rowsPerPage: _rowsPerPage,
+          onRowsPerPageChanged: (int? value) {
+            setState(() {
+              _rowsPerPage = value!;
+            });
+          },
+          sortColumnIndex: _sortColumnIndex,
+          sortAscending: _sortAscending,
+          onSelectAll: _doctorsDataSource.selectAll,
+          availableRowsPerPage: const <int>[10,20,40,60],
+
+          columns: <DataColumn>[
+            DataColumn(
+              label: const Text('Nom Complet'),
+              onSort: (int columnIndex, bool ascending) => _sort<String>(
+                      (Doctor d) => d.name, columnIndex, ascending),
+            ),
+            DataColumn(
+              label: const Text('Sex'),
+              //numeric: true,
+              onSort: (int columnIndex, bool ascending) => _sort<String>(
+                      (Doctor d) => d.sex, columnIndex, ascending),
+            ),
+            DataColumn(
+              label: const Text('Speciality'),
+              onSort: (int columnIndex, bool ascending) => _sort<String>(
+                      (Doctor d) => d.speciality, columnIndex, ascending),
+            ),
+            DataColumn(
+              label: const Text('Hospital'),
+              //tooltip: 'Each hospital.',
+              onSort: (int columnIndex, bool ascending) => _sort<String>(
+                      (Doctor d) => d.hospital, columnIndex, ascending),
+            ),
+            DataColumn(
+              label: const Text('Location'),
+              tooltip: 'Hospital location.',
+              onSort: (int columnIndex, bool ascending) => _sort<String>(
+                      (Doctor d) => d.location, columnIndex, ascending),
+            ),
+            DataColumn(
+              label: const Text('Phone'),
+              onSort: (int columnIndex, bool ascending) => _sort<String>(
+                      (Doctor d) => d.phoneNumbers.first,
+                  columnIndex,
+                  ascending),
+            ),
+            DataColumn(
+              label: const Text('Email'),
+              //tooltip: '',
+              onSort: (int columnIndex, bool ascending) => _sort<String>(
+                      (Doctor d) => d.emails.first, columnIndex, ascending),
+            ),
+            DataColumn(
+              label: const Text('Doctor'),
+              numeric: true,
+              onSort: (int columnIndex, bool ascending) => _sort<String>(
+                      (Doctor d) => d.isDoctor.toString(),
+                  columnIndex,
+                  ascending),
+            ),
+          ],
+          source: _doctorsDataSource,
         ),
       ),
     );
