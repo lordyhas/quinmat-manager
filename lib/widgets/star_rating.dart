3695fda9966e22b7efee5aebde08175dd79e7326
lordyhas <dev.haspro@gmail.com>
2023-06-27 06:54:02 +0200

    version 0.1.8+12 | to fluent

--- lib/widgets/star_rating.dart
+++ lib/widgets/star_rating.dart
@@ -1,11 +1,12 @@
-import 'package:flutter/material.dart';
 
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/cupertino.dart' show CupertinoIcons;
 class StarRating extends StatelessWidget {
   final int starCount;
   final bool allowHalfRating;
   final double rating;
   final double size;
-  final Color color;
+  final Color? color;
   final Color? borderColor;
 
   const StarRating({
@@ -14,7 +15,7 @@ class StarRating extends StatelessWidget {
       this.allowHalfRating = true,
       this.rating = 0,
       this.size = 15,
-      this.color = Colors.amber,
+      this.color,
       this.borderColor
     }) : super(key: key);
 
@@ -25,14 +26,12 @@ class StarRating extends StatelessWidget {
         double value = rating - index.toDouble();
         //print('StarRating(index => $index : $value) ####');
         if(value > 1) {
-          return Icon(Icons.star, color: color, size: size,);
+          return Icon(CupertinoIcons.star_fill, color: color ?? Colors.yellow, size: size,);
         } else if(value > 0 && value < 1){
-          return Icon(Icons.star_half_outlined, color: color, size: size,);
+          return Icon(CupertinoIcons.star_lefthalf_fill, color: color, size: size,);
+        } else {
+          return Icon(CupertinoIcons.star, color: color, size: size,);
         }
-        else {
-          return Icon(Icons.star_border, color: color, size: size,);
-        }
-
       }) ,
     );
   }
