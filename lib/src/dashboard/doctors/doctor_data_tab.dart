a9de51725340cae3347f6dac2c3300dc7a10ee13
lordyhas <dev.haspro@gmail.com>
2023-06-28 06:55:21 +0200

    version 0.1.8+14

--- lib/src/dashboard/doctors/doctor_data_tab.dart
+++ lib/src/dashboard/doctors/doctor_data_tab.dart
@@ -6,23 +6,6 @@ import 'package:qmt_manager/logic/data.csv.dart';
 import '../../../logic/transfer_protocol/http_protocol.dart';
 import '../../../logic/values.dart';
 
-/*
-class _Dessert {
-  _Dessert(this.name, this.calories, this.fat, this.carbs, this.protein,
-      this.sodium, this.calcium, this.iron);
-
-  final String name;
-  final int calories;
-  final double fat;
-  final int carbs;
-  final double protein;
-  final int sodium;
-  final int calcium;
-  final int iron;
-
-  bool? selected = false;
-}
-*/
 
 enum ContactLabel { mobile, work }
 
