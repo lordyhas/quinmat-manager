ba548b93f816f5a491af42dea7142d87fcb93f97
lordyhas <dev.haspro@gmail.com>
2023-08-07 21:04:03 +0200

    version 0.1.9+1

--- lib/src/dashboard/customers/product.dart
+++ lib/src/dashboard/customers/product.dart
@@ -2,11 +2,8 @@ library products;
 
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/cupertino.dart' show CupertinoIcons;
-import 'package:qmt_manager/logic/values.dart';
-import 'package:qmt_manager/widgets/widgets.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:intl/intl.dart' show DateFormat;
 
+import 'package:qmt_manager/widgets/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../../logic/model/data_model.dart';
@@ -14,9 +11,4 @@ import '../../../logic/model/data_model.dart';
 export 'custom_calendar.dart';
 export 'calendar_popup_view.dart';
 
-//part 'model/popular_filter_lmodel.dart';
-//part 'model/item_model.dart';
-
 part 'product_list_view.dart';
-//part 'slider_view.dart';
-//part 'range_slider_view.dart';
