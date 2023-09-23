18f5ced7005c0e6ec9a7ededdfb973bfe98ff3ea
lordyhas <dev.haspro@gmail.com>
2023-09-23 20:00:05 +0200

    add auth value to access controller | version 0.1.11+1

--- lib/logic/access_controller/access_controller_state.dart
+++ lib/logic/access_controller/access_controller_state.dart
@@ -1,14 +1,30 @@
 part of 'access_controller_cubit.dart';
 
 
-enum AccessLevel {admin, lvl3, lvl2, lvl1, zero, unknown}
+enum AccessLevel {admin, sudo, lvl2, lvl3, zero, unknown}
 
 class AccessControllerState {
-  final AccessLevel level;
+  final AccessLevel _level;
 
-  const AccessControllerState._(this.level);
+  const AccessControllerState._(this._level);
   const AccessControllerState.initial() : this._(AccessLevel.unknown);
   const AccessControllerState.change({required AccessLevel level}) : this._(level);
+
+
+}
+
+extension AccessLevelHelper on AccessLevel {
+  int get value {
+    switch(this){
+      case AccessLevel.admin: return 0;
+      case AccessLevel.lvl3: return 3;
+      case AccessLevel.lvl2: return 2;
+      case AccessLevel.sudo: return 1;
+      case AccessLevel.zero: return 0;
+      case AccessLevel.unknown: return -1;
+    }
+
+  }
 }
 
 
